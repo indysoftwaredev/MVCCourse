0455d298600c5f15711c18179d8d99a0062fb58d
David Carter <indysoftwaredev@gmail.com>
2024-08-24 11:24:44 -0400

    - Add in-memory data repository

---
+++ WebApp/Models/CategoriesRepository.cs
@@ -0,0 +1,59 @@
+﻿namespace WebApp.Models
+{
+    public class CategoriesRepository
+    {
+        private static List<Category> _categories = new List<Category>()
+        {
+            new Category { CategoryId=1, Description="Beverage", Name="Meat" },
+            new Category { CategoryId=2, Description="Bakery", Name="Bakery" },
+            new Category { CategoryId=3, Description="Meat", Name="Meat"}
+        };
+
+        public static void AddCategory(Category category)
+        {
+            int maxId = _categories.Max(c => c.CategoryId);
+            category.CategoryId = maxId;
+            _categories.Add(category);
+        }
+
+        public static List<Category> GetCategories() => _categories;
+
+        public static Category? GetCategory(int categoryId)
+        {
+            var category = _categories.FirstOrDefault(c => c.CategoryId == categoryId);
+
+            if(category != null)
+            {
+                return new Category
+                {
+                    CategoryId = category.CategoryId,
+                    Description = category.Description,
+                    Name = category.Name
+                };
+            }
+
+            return null;
+        }
+
+        public static void UpdateCategory(int categoryId, Category category)
+        {
+            if (categoryId != category.CategoryId) return;
+
+            var categoryUpdate = GetCategory(categoryId);
+            if (categoryUpdate != null)
+            {
+                categoryUpdate.Name = category.Name;
+                categoryUpdate.Description = category.Description;
+            }
+        }
+
+        public static void DeleteCategory(int categoryId)
+        {
+            var categoryDelete = _categories.FirstOrDefault(c => c.CategoryId == categoryId);
+            if( categoryDelete != null )
+            {
+                _categories.Remove(categoryDelete);
+            }
+        }
+    }
+}
