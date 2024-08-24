12be99cb52f5ad29b3b3fdeeb0c2fea8644c3338
David Carter <indysoftwaredev@gmail.com>
2024-08-24 12:17:12 -0400

    - Add Model Binding

--- WebApp/Controllers/CategoriesController.cs
+++ WebApp/Controllers/CategoriesController.cs
@@ -11,9 +11,9 @@ namespace WebApp.Controllers
             return View(categories);
         }
 
-        public IActionResult Edit(int? id)
+        public IActionResult Edit([FromRoute]int? id)
         {
-            var category = new Category { CategoryId = id.HasValue ? id.Value : 0 };
+            var category = CategoriesRepository.GetCategory(id.HasValue ? id.Value : 0);
 
             return View(category);
         }
