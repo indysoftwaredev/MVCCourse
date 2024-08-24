02e31004420812f53d11140d70b4df2813bd6f66
David Carter <indysoftwaredev@gmail.com>
2024-08-24 13:00:38 -0400

    - Add form validation.

--- WebApp/Models/Category.cs
+++ WebApp/Models/Category.cs
@@ -1,9 +1,15 @@
-﻿namespace WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
 {
     public class Category
     {
         public int CategoryId { get; set; }
+
+        [Required]
         public string Name { get; set; } = string.Empty;
+
+        [Required]
         public string Description { get; set; } = string.Empty;
     }
 }
